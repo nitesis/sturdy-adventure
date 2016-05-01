60720c508ab0b42e5b7c1a043ea540352b323140
souzanH <souzanH@users.noreply.github.com>
2016-05-01 02:31:56 -1200

    Init Project

---
+++ Android_Labyrinth/app/src/test/java/ch/fhnw/android_labyrinth/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package ch.fhnw.android_labyrinth;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
